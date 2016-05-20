9d19f91370ed59be5472e10ca380bb107b8d14fc
Maxim Buzdalov <mbuzdalov@gmail.com>
2016-05-20 17:19:07 +0300

    Now PASL interaction works. Surprisingly (?), OpenMP is better.

--- example/rc-tree/looping_driver_pasl.cpp
+++ example/rc-tree/looping_driver_pasl.cpp
@@ -10,7 +10,7 @@ void looping_driver_pasl::loop_for(int from, int until, std::function<void(int)>
 void looping_driver_pasl::compute_prefix_sum(int from, int until, std::function<int&(int)> value, std::function<int&(int)> result) {
     int jump = 1;
     while (jump < until - from) {
-        pasl::sched::native::parallel_for(0, (until - from + 2 * jump - 1) / (2 * jump), [&] (int j) -> void {
+        pasl::sched::native::parallel_for(0, (until - from + jump - 1) / (2 * jump), [from, value, jump] (int j) -> void {
             int i = j * 2 * jump + from;
             value(i) += value(i + jump);
         });
@@ -19,7 +19,7 @@ void looping_driver_pasl::compute_prefix_sum(int from, int until, std::function<
     result(from) = value(from);
     while (jump > 1) {
         jump /= 2;
-        pasl::sched::native::parallel_for(0, (until - from + 2 * jump - 1) / (2 * jump), [&] (int j) -> void {
+        pasl::sched::native::parallel_for(0, (until - from + jump - 1) / (2 * jump), [from, value, result, jump] (int j) -> void {
             int i = j * 2 * jump + from;
             int z = value(i + jump);
             value(i) -= z;
