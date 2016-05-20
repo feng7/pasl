0f131bb5ae966622fecccc98dfbebb233f1af08b
Maxim Buzdalov <mbuzdalov@gmail.com>
2016-05-20 13:04:22 +0300

    PASL support now compiles (via Makefile) but crashes at the very beginning

--- example/rc-tree/looping_driver_pasl.cpp
+++ example/rc-tree/looping_driver_pasl.cpp
@@ -13,7 +13,7 @@ void looping_driver_pasl::compute_prefix_sum(int from, int until, std::function<
         pasl::sched::native::parallel_for(0, (until - from + 2 * jump - 1) / (2 * jump), [&] (int j) -> void {
             int i = j * 2 * jump + from;
             value(i) += value(i + jump);
-        }
+        });
         jump += jump;
     }
     result(from) = value(from);
@@ -25,6 +25,6 @@ void looping_driver_pasl::compute_prefix_sum(int from, int until, std::function<
             value(i) -= z;
             result(i + jump) = result(i);
             result(i) -= z;
-        }
+        });
     }
 }
