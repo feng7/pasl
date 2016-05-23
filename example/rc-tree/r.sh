62a91bc9d7e53424e4924949e0609fd31f1ed215
Maxim Buzdalov <mbuzdalov@gmail.com>
2016-05-23 04:25:37 +0300

    Attempt to create a 'portable' tcmalloc linkage

--- example/rc-tree/r.sh
+++ example/rc-tree/r.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+TCMALLOC_TEST_PRELOAD=
+
+function try_compiling_with_tcmalloc() {
+    g++ -std=c++11 -O3 -Wall -Wextra -fopenmp -ltcmalloc -o main "$@"
+    if [[ "$?" == "0" ]]; then
+        echo "Compilation was OK with -ltcmalloc"
+        TCMALLOC_TEST_PRELOAD=
+    else
+        echo "Compilation failed with -ltcmalloc"
+        g++ -std=c++11 -O3 -Wall -Wextra -fopenmp -l:libtcmalloc.4.so -o main "$@"
+        if [[ "$?" == "0" ]]; then
+            echo "Compilation was OK with -l:libtcmalloc.4.so"
+            TCMALLOC_TEST_PRELOAD="LD_PRELOAD=/usr/lib/libtcmalloc.so.4"
+        else
+            echo "Compilation failed with -l:libtcmalloc.4.so"
+            exit 1
+        fi
+    fi
+}
+
 if [[ "$1" == "test" ]]; then
 
     g++ -std=c++11 -O3 -Wall -Wextra -o test-dc \
@@ -41,15 +61,30 @@ if [[ "$1" == "test" ]]; then
 
 elif [[ "$1" == "perf" ]]; then
 
-    g++ -std=c++11 -O3 -Wall -Wextra -fopenmp -ltcmalloc -o main \
+    try_compiling_with_tcmalloc \
         dynamic_connectivity.cpp \
         thread_local_random.cpp \
         timing_$2.cpp
 
     if [[ "$3" == "" ]]; then
-        ./main
+        $TCMALLOC_TEST_PRELOAD ./main
     else
-        OMP_NUM_THREADS=$3 ./main
+        OMP_NUM_THREADS=$3 $TCMALLOC_TEST_PRELOAD ./main
+    fi
+    rm main
+
+elif [[ "$1" == "grind" ]]; then
+
+
+    try_compiling_with_tcmalloc \
+        dynamic_connectivity.cpp \
+        thread_local_random.cpp \
+        timing_$2.cpp
+
+    if [[ "$3" == "" ]]; then
+        $TCMALLOC_TEST_PRELOAD valgrind --tool=callgrind --cache-sim=yes ./main
+    else
+        OMP_NUM_THREADS=$3 $TCMALLOC_TEST_PRELOAD valgrind --tool=callgrind --cache-sim=yes ./main
     fi
     rm main
 
